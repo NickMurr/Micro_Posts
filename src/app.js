8ce234f91df677704bd5107df42eb36f8998fbb4
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-08-14 14:23:07 +0300

    Add Posts and Showing Alert

--- src/app.js
+++ src/app.js
@@ -4,8 +4,33 @@ import { ui } from './ui';
 // Get Posts on DOM Load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+// Add Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body,
+  };
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post Added', 'alert alert-success');
+      ui.clearFields();
+
+      getPosts();
+    })
     .catch(err => console.log(err));
 }
